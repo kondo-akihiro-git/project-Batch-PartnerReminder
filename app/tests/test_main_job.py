1fff0d43f88d46547762b37bce3757185e915bbe
kondo-akihiro-git <2000akisan@gmail.com>
2025-05-20 19:09:06 +0900

    バッチのユーザ取得追加

--- app/tests/test_main_job.py
+++ app/tests/test_main_job.py
@@ -1,6 +1,7 @@
 from app.jobs.main_job import main_job
 
-def test_main_job(capsys):
-    main_job()
-    captured = capsys.readouterr()
-    assert "明日デートの予定があるユーザーはいません" in captured.out
+def test_main_job(caplog):
+    with caplog.at_level("INFO"):
+        main_job()
+    # caplog.textにログ出力全体が入るのでそれをチェック
+    assert "明日デートの予定があるユーザーはいません" or "メール送信完了" in caplog.text
