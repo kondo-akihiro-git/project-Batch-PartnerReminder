c824485c19b0798d896e9565113a1e8f0eb9a066
kondo-akihiro-git <2000akisan@gmail.com>
2025-05-21 10:19:39 +0900

    env化対応

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,11 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.jobs.main_job import main_job
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def init_scheduler():
     scheduler = BackgroundScheduler()
 
+    interval_seconds = int(os.getenv("SCHEDULER_INTERVAL_SECONDS", "10"))
+
     # 現在の日付がデートの前日か判定してメール送信するジョブ
-    scheduler.add_job(main_job, 'interval', seconds=10, id='main_job')
+    scheduler.add_job(main_job, 'interval', seconds=interval_seconds, id='main_job')
 
     scheduler.start()
     return scheduler
